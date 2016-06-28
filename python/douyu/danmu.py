4836f252109c8fe5c59c3956008b8347235c7633
Ray Chen <chenrano2002@gmail.com>
2016-06-29 01:14:11 +0800

    got danmu success

--- python/douyu/danmu.py
+++ python/douyu/danmu.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
+# encoding: utf-8
 
-import re
+import time
 import socket
 import logging
+import threading
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-DANMU_SERVER = 'openbarrange.douyutv.com'
+DANMU_SERVER = 'openbarrage.douyutv.com'
 DANMU_PORT = 8601
 
-log = logging.getLogger(__name__) 
+log = logging.getLogger(__name__)
 
 
 class InvalidParam(Exception):
@@ -20,7 +25,7 @@ class ResponseError(Exception):
 
 
 class DanMuResponse(object):
-    MSG_MAP = {
+    MSG_ERROR_MAP = {
         '0': 'Success',
         '51': 'Data transport error',
         '52': 'The Server is closed',
@@ -28,42 +33,85 @@ class DanMuResponse(object):
     }
 
     def __init__(self, response):
-        self.content = response
+        self.content = str(response)
 
     @property
     def data(self):
-        _data = []
+        _data = {}
         parts = self.content.split("/")
         for p in parts:
             if p:
                 (key, value) = p.split('@=')
-                _data.append({key: value})
+                _data[key] = value
         log.debug("response data: %s" % _data)
         return _data
+
+    def __getattr__(self, item):
+        try:
+            return self.data[item]
+        except KeyError:
+            log.error("Key '%s' is not found in response" % item)
+            return ""
 
     def __repr__(self):
         return repr(self.data)
 
     def has_error(self):
-        return 'error' in self.content
+        return 'error' in self.data
 
-    def check(self):
+    def raise_if_error(self):
         if not self.has_error():
             return True
         # if error found, generate related exception
-        code = self.data[1]['code']
-        if code not in self.MSG_MAP:
+        code = self.code
+        if code not in self.MSG_ERROR_MAP:
             msg = 'Unknown Error'
         else:
-            msg = self.MSG_MAP[code]
+            msg = self.MSG_ERROR_MAP[code]
         raise ResponseError(msg)
+
+    def _show_chat_msg(self):
+        message = "\t".join([self.type, self.nn]) + '<' + self.uid + \
+                  self.level + '>\t' + self.txt
+        log.info(message)
+
+    def _show_user_enter(self):
+        message = "\t".join([self.type, self.nn]) + '<' + self.uid + \
+                  self.level + '>\t' + "Enter this room"
+        log.info(message)
+
+    def _show_gift(self):
+        message = self.type + self.gn + "<" + self.gc + ">\t" + \
+                  "\t".join[self.sn, self.dn, self.drid]
+        log.info(message)
+
+    def _show_rank_list(self):
+        log.info("TODO")
+
+    def display(self):
+        if self.type == 'chatmsg':
+            #log.info("Type NickName<uid, level> Message")
+            self._show_chat_msg()
+        elif self.type == 'uenter':
+            self._show_user_enter()
+        elif self.type == 'spbc':
+            #log.info("Type Gift<Number> Sender Receiver Room")
+            self._show_gift()
+        elif self.type == 'ranklist':
+            self._show_rank_list()
+        else:
+            raise ResponseError("Unknown message type: %s" % msg_type)
 
 
 class DanMuMsg(object):
+    """dou yu protocol message with little byteorder"""
     def __init__(self, content, is_server=False):
         self.length = bytearray([len(content) + 9, 0x00, 0x00, 0x00])
         self.code = self.length
         self.magic = bytearray([0xb1, 0x02, 0x00, 0x00])
+        # client code is 689, server code is 690
+        if is_server:
+            self.magic = bytearray([0xb2, 0x02, 0x00, 0x00])
         self.content = bytes(content.encode("utf-8"))
         self.end = bytearray([0x00])
 
@@ -96,11 +144,11 @@ class DanMuClient(object):
 
     @staticmethod
     def _convert(msg):
+        msg = str(msg)
         # if contain '@', then convert to '@A'
         msg = msg.replace('@', '@A')
         # if contain '/', then convert to '@S'
         msg = msg.replace('/', '@S')
-        log.debug('converted msg: %s' % msg)
         return msg
 
     def _build_msg(self, **kwargs):
@@ -118,24 +166,27 @@ class DanMuClient(object):
         log.debug('built message is %s' % msg)
         return msg
 
-    def _parse_response(self, msg):
+    def _parse_response(self):
+        msg = self._session.recv(1024)
         content = msg[12:-1].decode('utf-8', 'ignore')
-        res = DanMuResponse(content)
-        res.check()
+        response = DanMuResponse(content)
+        response.raise_if_error()
+        return response
 
-    def login(self, room_id):
-        msg = self._build_msg(type='logreq', roomid=str(room_id))
+    def login(self, room_id, username="", password=""):
+        #msg = self._build_msg(type='loginreq', username=username,
+        #                      password=password, roomid=str(room_id))
+        msg = 'type@=loginreq/roomid@=/%s/' % room_id
         self.send(msg)
         # check the response from server
-        res = self._session.recv(2048)
-        self._parse_response(res)
+        self._parse_response()
 
     def logout(self):
         msg = self._build_msg(type='logout')
         self.send(msg)
 
-    def send(self, text, is_server=False):
-        msg = DanMuMsg(text, is_server)
+    def send(self, text):
+        msg = DanMuMsg(text)
         log.debug('start send message: %s' % text)
         try:
             self._session.sendall(msg.bytes)
@@ -143,17 +194,51 @@ class DanMuClient(object):
             log.debug("Send message failed with %s" % e)
 
 
-    def heartbeat(self, interval=30):
-        msg = self._build_msg(type='keeplive', tick=time())
+    def heartbeat(self, interval=10):
+        msg = self._build_msg(type='keeplive', tick=int(time.time()))
         self.send(msg)
-        self._is_live = True
+        try:
+            self._parse_response()
+            self._is_live = True
+        except ResponseError as error:
+            log.error("Found heart beat error: %s" % error)
+            self._is_live = False
+
+        if self._is_live:
+            log.debug("hear beat is ok, retry in %d second" % interval)
+            time.sleep(interval)
 
     def join_group(self, rid, gid=-9999):
         msg = self._build_msg(type='joingroup', rid=rid, gid=gid)
         self.send(msg)
+        self._parse_response()
+
+    def run(self, room_id):
+        url = "http://www.douyuyv.com/" + str(room_id)
+        log.info("Show Danmu in Room: '%s'" % url)
+
+        self.login(room_id)
+
+        # start new thread to keep monitor heartbeat
+        t = threading.Thread(target=self.heartbeat)
+        #t.setDaemon(True)
+        t.start()
+        self.join_group(room_id)
+
+        # Now keeping get the danmu message
+        while True:
+            message = self._parse_response()
+            message.display()
+
+        log.info("Finish run")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    danmu = DanMuClient(server='122.188.107.179')
-    danmu.login(48699)
+    logging.basicConfig(level=logging.INFO,
+            format='%(asctime)s %(name)-8s %(levelname)-6s %(message)s',
+            filename='test.log',
+            datefmt = '%m-%d %H:%M')
+
+    danmu = DanMuClient()
+    danmu.run(501999)
+
